5009d0edecf1088252cbcd6ea233e05b08b4b9b8
Evan Anderson <evananderson@Evans-MacBook-Pro.local>
2016-11-15 23:44:02 -0600

    tried adding some more code to get gifs and buttons to appear

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,29 +6,39 @@
 
 
 	// var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=starwars";
+function displayGifInfo(){
+	var gif = $(this).attr("data-name");
+	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + gif + "&api_key=dc6zaTOxFJmzC&limit=10";
 
+	//creates ajax
+	$.ajax({url: queryURL, method: 'GET'}).done(function(response){
+		$("img").remove(); //remove the classes to resest the container
+         $("p").remove();
+		var results = response.data;
+		//var gifDiv = $("<div class='gif'>");
+		var rating = response.rating;
 
-	 var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + person + "&api_key=dc6zaTOxFJmzC&limit=10";
+        for (var i = 0; i < results.length; i++) {
+            var gifDiv = $("<div class='item'>")
 
-	$.ajax({url: queryURL, method: 'GET'}) 
-	.done(function(response) {
-	var results = responce.data;
+            var rating = results[i].rating;
 
+            var p = $("<p>").text("Rating: " + rating);
 
-            var gifDiv = $('<div class="item">')
+           var personImage = $("<img>");
+            personImage.attr("src", results[i].images.fixed_height_still.url);
+            personImage.attr("data-still", results[i].images.fixed_height_still.url);
+            //personImage.attr("data-state","still");
+            personImage.addClass("item");
+            personImage.attr("data-animate", results[i].images.fixed_height.url);
 
-                var rating = results[i].rating;
+            gifDiv.append(p)
+            gifDiv.append(personImage)
 
-                var p = $('<p>').text( "Rating: " + rating);
-
-                var personImage = $('<img>');
-                personImage.attr('src', results[i].images.fixed_height.url);
-
-                gifDiv.append(p)
-                gifDiv.append(personImage)
-
-                $('#gifView').prepend(gifDiv);
-            })
+            $("#gifView").prepend(gifDiv);
+        }
+	});
+}
 
 
 
@@ -55,14 +65,14 @@
 
 		}
 
-	};
+	}
 
-	$("#findGalaxy").on('click', function(){
-
+	$("#findGalaxy").on('click', function(event){
+			event.preventDefault();
 		// Here we grab the text from the input box 
-		var starwars = $("#buttonsView").val().trim();
+		var starWars = $("#buttonsView").val().trim();
 		console.log("#galaxyInput");
-			starWarsGif.push(starWarsGif);
+			buttonsView.push(buttonsView);
 
 			
 
@@ -74,13 +84,13 @@
 
 		//------
 
-		return false;
+		
 	})
 
 
-	document.on("click", "#buttonsView", displayRenderButtons);
+	// document.on("click", "#buttonsView", displayRenderButtons);
 
-	renderButtons();
+	
 
 
 
